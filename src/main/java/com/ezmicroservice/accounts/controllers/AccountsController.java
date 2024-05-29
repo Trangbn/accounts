c66b58d050b0f52385e2bb382c59b8cf471ad03c
trangbn <trangbn580@gmail.com>
2024-05-29 22:45:44 +0700

    adding documentation swagger for account API

--- src/main/java/com/ezmicroservice/accounts/controllers/AccountsController.java
+++ src/main/java/com/ezmicroservice/accounts/controllers/AccountsController.java
@@ -4,6 +4,10 @@ import com.ezmicroservice.accounts.constants.AccountsConstants;
 import com.ezmicroservice.accounts.dto.CustomerDto;
 import com.ezmicroservice.accounts.dto.ResponseDto;
 import com.ezmicroservice.accounts.service.IAccountsService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
@@ -18,10 +22,22 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @AllArgsConstructor
 @Validated
+@Tag(
+        name = "API microservice API",
+        description = "Practice Microservice API"
+)
 public class AccountsController {
 
     private IAccountsService accountsService;
 
+    @Operation(
+            summary = "Create new accounts",
+            description = "create new accounts"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP status CREATED"
+    )
     @PostMapping("/create")
     public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) {
 
@@ -30,6 +46,14 @@ public class AccountsController {
                 .body(new ResponseDto(AccountsConstants.STATUS_201, AccountsConstants.MESSAGE_201));
     }
 
+    @Operation(
+            summary = "Get accounts information",
+            description = "Get accounts info"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP status fetched successfully"
+    )
     @GetMapping("/fetch")
     public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam
                         @Pattern(regexp = "(^$|[0-9]{10})", message = "Phone number should be 10 digits")
@@ -38,6 +62,20 @@ public class AccountsController {
         return ResponseEntity.status(HttpStatus.OK).body(customerDto);
     }
 
+    @Operation(
+            summary = "Update accounts",
+            description = "update accounts"
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "HTTP status updated"
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal server error"
+            )
+    })
     @PutMapping("/update")
     public ResponseEntity<ResponseDto> updateAccountsDetails(@Valid @RequestBody CustomerDto customerDto) {
         boolean isUpdated = accountsService.updateAccount(customerDto);
@@ -46,6 +84,14 @@ public class AccountsController {
                 : ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ResponseDto(AccountsConstants.STATUS_500, AccountsConstants.MESSAGE_500));
     }
 
+    @Operation(
+            summary = "Delete accounts",
+            description = "Delete accounts"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP status deleted successfully"
+    )
     @DeleteMapping("/delete")
     public ResponseEntity<ResponseDto> deleteAccountDetails( @RequestParam
                                                                 @Pattern(regexp = "(^$|[0-9]{10})", message = "Phone number should be 10 digits")
