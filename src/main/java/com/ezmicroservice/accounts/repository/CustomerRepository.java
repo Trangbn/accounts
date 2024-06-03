c7aa1b94f2e0bc8b46ab15e9fe7563ac0ab69716
trangbn <trangbn580@gmail.com>
2024-06-03 17:29:26 +0700

    Update for repository and docker

--- src/main/java/com/ezmicroservice/accounts/repository/CustomerRepository.java
+++ src/main/java/com/ezmicroservice/accounts/repository/CustomerRepository.java
@@ -1,6 +1,5 @@
 package com.ezmicroservice.accounts.repository;
 
-import com.ezmicroservice.accounts.dto.CustomerDto;
 import com.ezmicroservice.accounts.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
